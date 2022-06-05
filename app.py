65cfa356049c37c4cfbeecaede75eef1e4303d2c
Muhammad Faizan Naeem <84436536+muhammadfaizannaheem@users.noreply.github.com>
2022-06-05 12:45:05 +0500

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ CORS(app)
 # loading the saved model from local drive
 model=joblib.load('model_jbl.model')
 
-@app.route('/predictUnnamed/<float:v_1>/<int:v_2>/<int:v_3>/<int:v_4>/<int:v_5>/<int:v_6>',methods = ['GET','POST'])
+@app.route('/predictUnnamed/<int:v_1>/<int:v_2>/<int:v_3>/<int:v_4>/<int:v_5>/<int:v_6>',methods = ['GET','POST'])
 def predict_unnamed(v_1,v_2,v_3,v_4,v_5,v_6):
     """This function gets the unnamed args from the api and predicts the label"""
     prediction = model.predict([[v_1,v_2,v_3,v_4,v_5,v_6]])
